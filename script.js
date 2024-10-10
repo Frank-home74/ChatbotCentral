904a4b379c858170eb2c971ce094e67018f57c0d
Frank-home74 <frankhaa@hiof.no>
2024-10-10 13:20:10 +0200

    oppdatering 2

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const hamburgerMenu = document.getElementById('hamburgerMenu');
+        const navList = document.getElementById('navList');
+
+        hamburgerMenu.addEventListener('click', function() {
+            navList.classList.toggle('active');
+        });
+    });
+</script>
